eb0f948cd0111f408350bff452c52c1bb6de2ebb
punkestu <pangestubima89@gmail.com>
2026-04-27 14:27:03 +0700

    fix: make it installable

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,35 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        "icons": [
+          {
+            "src": "/192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ],
+        "screenshots": [
+          {
+            "src": "/page-desktop.png",
+            "sizes": "1280x720",
+            "type": "image/png",
+            "form_factor": "wide"
+          },
+          {
+            "src": "/page-mobile.png",
+            "sizes": "390x844",
+            "type": "image/png"
+          }
+        ]
       }
     }),
     react(),
